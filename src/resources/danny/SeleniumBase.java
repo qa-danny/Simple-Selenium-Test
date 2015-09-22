daab0d2888afab53ebb3ddcb5a33e4dfda4a3f0e
Schaich <dschaich@a45e60e18f47.ant.amazon.com>
2015-09-22 14:09:36 -0700

    adding work to project

---
+++ src/resources/danny/SeleniumBase.java
@@ -0,0 +1,69 @@
+package resources.danny;
+
+import java.util.NoSuchElementException;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
+
+public class SeleniumBase {
+
+	protected static WebDriver driver;
+	protected static String baseUrl;
+	
+	/**
+	 * BeforeClass creates the driver and has it set for the entire duration
+	 *  of the test class.
+	 * @throws Exception
+	 */
+	
+	@BeforeClass
+	public static void setUp() throws Exception {
+		FirefoxProfile profile = new FirefoxProfile();
+		profile.setAcceptUntrustedCertificates(true);
+		profile.setAssumeUntrustedCertificateIssuer(false);
+		
+		baseUrl = "http://www.6pm.com";
+		driver = new FirefoxDriver(profile);
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	}
+	
+	/**
+	 * AfterClass tears down the driver, ending the tests. Hence the 
+	 *  name, tearDown.
+	 * @throws Exception
+	 */
+	@AfterClass
+    public static void tearDown() throws Exception {
+           driver.quit();
+    }
+
+    public boolean isElementPresent(By by) {
+           try {
+                  driver.findElement(by);
+                  return true;
+           } catch (NoSuchElementException e) {
+                  return false;
+           }
+    }
+	
+	public static void jQueryWait(WebDriver driver) {
+		int i = 0;
+		try {
+			do {
+		        Thread.sleep(300);
+		        i++;
+		        if(i == 50){
+	                driver.navigate().refresh();
+	            }
+		    }
+		    while (!((JavascriptExecutor)driver).executeScript("return document.readyState").equals("complete") && i < 100);
+		} catch (InterruptedException e) {}
+	}
+    
+}
---
+++ src/tests/danny/ZapposSearch.java
@@ -0,0 +1,14 @@
+package tests.danny;
+
+import org.junit.Test;
+
+import resources.danny.SeleniumBase;
+
+public class ZapposSearch extends SeleniumBase {
+	
+	@Test
+	public void ZapposSearch_Test() {
+		
+	}
+
+}
